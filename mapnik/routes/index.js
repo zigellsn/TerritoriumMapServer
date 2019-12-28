d5a32256bd2b366939d335fef94723b65e788047
Simon Zigelli <zigellsn@gmail.com>
2019-12-28 22:45:24 +0100

    Use query parameters from request for rendering

--- mapnik/routes/index.js
+++ mapnik/routes/index.js
@@ -4,12 +4,46 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/territorium/map', function (req, res, next) {
     let renderer = new Renderer();
     try {
-        renderer.worldfile(9.15, 48.7, 9.16, 48.8, 0, 0, 256, 256, function (file) {
-            console.log(file);
+        let xrot = 0;
+        if (req.query.xrotation !== undefined)
+            xrot = req.query.xrotation;
+
+        let yrot = 0;
+        if (req.query.yrotation !== undefined)
+            yrot = req.query.yrotation;
+
+        let imgtype = 'png';
+        if (req.query.type !== undefined)
+            imgtype = req.query.type;
+
+        renderer.map(req.query.long0, req.query.lat0, req.query.long1, req.query.lat1, xrot, yrot, req.query.width, req.query.height, imgtype, function (file) {
         });
-        renderer.map(9.15, 48.7, 9.16, 48.8, 0, 0, 256, 256, 'png', function (file) {
+    } catch (e) {
+        console.log(e);
+    }
+    res.render('index', {title: 'Express'});
+});
+
+
+router.get('/territorium/worldfile', function (req, res, next) {
+    let renderer = new Renderer();
+    try {
+        let xrot = 0;
+        if (req.query.xrotation !== undefined)
+            xrot = req.query.xrotation;
+
+        let yrot = 0;
+        if (req.query.yrotation !== undefined)
+            yrot = req.query.yrotation;
+
+        renderer.worldfile(req.query.long0, req.query.lat0, req.query.long1, req.query.lat1, xrot, yrot, req.query.width, req.query.height, function (file) {
+            console.log(file);
         });
     } catch (e) {
         console.log(e);
