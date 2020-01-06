2329410bdba08793d5ae9f51f7568220059a0ad0
Simon Zigelli <zigellsn@gmail.com>
2020-01-06 22:53:44 +0100

    Implement new JSON-based renderer

--- mapnik/renderer/renderer.js
+++ mapnik/renderer/renderer.js
@@ -7,16 +7,14 @@ const {createCanvas, Image} = require('canvas');
 const srs = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over';
 const copyright = '© OpenStreetMap contributors';
 
+const merc = new mapnik.Projection(srs);
+
 function Renderer() {
+    mapnik.register_system_fonts();
+    mapnik.register_default_input_plugins();
 }
 
 function prepareRenderer(long0, lat0, long1, lat1, imgx, imgy, callback) {
-
-    let merc = new mapnik.Projection(srs);
-
-    mapnik.register_system_fonts();
-    mapnik.register_default_input_plugins();
-
     let map = new mapnik.Map(imgx, imgy);
     map.load('/input/osm-de.xml', function (err, map) {
         let bbox = [long0, lat0, long1, lat1];
@@ -27,12 +25,6 @@ function prepareRenderer(long0, lat0, long1, lat1, imgx, imgy, callback) {
 }
 
 function prepareRendererSync(long0, lat0, long1, lat1, imgx, imgy) {
-
-    let merc = new mapnik.Projection(srs);
-
-    mapnik.register_system_fonts();
-    mapnik.register_default_input_plugins();
-
     let map = new mapnik.Map(imgx, imgy);
     map.loadSync('/input/osm-de.xml');
     let bbox = [long0, lat0, long1, lat1];
@@ -60,6 +52,83 @@ function addCopyrightTextVector(src, width, height) {
     let len = src.length;
     return `${src.slice(0, len - 7)}<text fill="#0" font-size="10" font-family="sans-serif" x="10" y="${height - 32}"><tspan dy="18.2" x="10">${copyright}</tspan></text>${src.slice(len - 7, len)}`;
 }
+
+Renderer.prototype.map = function (size, bbox, way, mimeType, styles, names) {
+    function getInline(names) {
+        let nameString = 'name,x,y\n';
+        for (let i = 0; i < names.length; i++) {
+            nameString += `"${names[i].text}",${names[i].position[0]},${names[i].position[1]}\n`
+        }
+        return nameString;
+    }
+
+    function createStyles(styles) {
+        let s = '<Map>';
+        for (let i = 0; i < styles.length; i++) {
+            s += `<Style name="${styles[i].name}">`;
+            s += ` <Rule><LineSymbolizer stroke="${styles[i].color}" stroke-width="${styles[i].width}" stroke-opacity="${styles[i].opacity}"/></Rule>`;
+            s += '</Style>';
+        }
+        s += '<Style name="names_style">';
+        s += ' <Rule>';
+        s += '  <TextSymbolizer face-name="DejaVu Sans Book" size="30" fill="white" halo-fill="black" halo-radius="2" >[name]</TextSymbolizer>';
+        s += ' </Rule>';
+        s += '</Style>';
+        s += '</Map>';
+        return s;
+    }
+
+    function addAdditionalLayers(m, way, styles, names) {
+        let s = createStyles(styles);
+        m.fromStringSync(s);
+        let ds = new mapnik.Datasource({
+            'type': 'geojson',
+            'inline': way
+        });
+        let ds_names = new mapnik.Datasource({
+            'type': 'csv',
+            'inline': getInline(names)
+        });
+        let layer = new mapnik.Layer('border');
+        layer.datasource = ds;
+        layer.srs = srs;
+        layer.styles = [styles[0].name];
+        m.add_layer(layer);
+
+        layer = new mapnik.Layer('names');
+        layer.datasource = ds_names;
+        layer.srs = srs;
+        layer.styles = ['names_style'];
+        m.add_layer(layer);
+    }
+
+    try {
+        let m = new mapnik.Map(size[0], size[1]);
+        m.loadSync('/input/osm-de.xml');
+        m.zoomToBox(bbox);
+
+        addAdditionalLayers(m, JSON.stringify(way), styles, names);
+        let src;
+        if (mimeType === 'image/xml+svg') {
+            if (!mapnik.supports.cairo) {
+                console.log('So sad... no Cairo');
+                return undefined;
+            }
+            let filename = uuidv4();
+            m.renderFileSync(filename, {format: 'SVG'});
+            src = fs.readFileSync(filename);
+            fs.unlinkSync(filename);
+            src = addCopyrightTextVector(src, m.width, m.height);
+        } else {
+            src = m.renderSync({format: 'PNG'});
+            src = addCopyrightTextRaster(src, m.width, m.height);
+        }
+        return src;
+    } catch (e) {
+        console.log(e);
+    }
+
+};
 
 Renderer.prototype.mapLegacy = function (long0, lat0, long1, lat1, width, height, imgtype) {
 
